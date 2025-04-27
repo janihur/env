0464c6db895e48c1a91e06ea13b95f5deddb7026
Jani Hur <webmaster@metsankulma.net>
2025-04-27 17:06:27 +0300

    node version manager (nvm)

---
+++ roles/swdev/files/activate-nvm.sh
@@ -0,0 +1,5 @@
+# activate NVM
+# source, don't run
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
